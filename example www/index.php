4718cfc6bc259763775d79214e48a6590d75659a
dave hale <davejlhale@yahoo.co.uk>
2013-03-18 20:01:04 +0000

    fixing missplaced index file/css dir

---
+++ example www/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use dh_mvc2\Application;
+
+require_once  'C:\Users\dave\Zend\workspaces\new framework\framework\dh_mvc2\Application.php';
+
+Application::dir_link('C:/Users/dave/Zend/workspaces/new framework/webapp2')->run();
+
